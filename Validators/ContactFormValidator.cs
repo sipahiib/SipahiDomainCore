557f3b0968b585ac4f828cc009c9ceba7f34d791
İbrahim <73345010+sipahiib@users.noreply.github.com>
2021-03-12 22:12:34 +0300

    Add files via upload

---
+++ Validators/ContactFormValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using SipahiDomainCore.Models;
+
+namespace SipahiDomainCore.Validators
+{
+    public class ContactFormValidator : AbstractValidator<MailModels>
+    {
+        public ContactFormValidator()
+        {
+            RuleFor(s => s.Email).NotEmpty().WithMessage("Email adresi giriniz").EmailAddress().WithMessage("Email formatında giriniz");
+            RuleFor(x => x.Message).NotEmpty().WithMessage("Yorum giriniz").Length(1, 250).WithMessage("Max 250 karakter giriniz.");
+        }
+    }
+}
