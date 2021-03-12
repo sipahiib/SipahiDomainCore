ecff4734043235f6cee4e857d1add3f513a4f2fb
İbrahim <73345010+sipahiib@users.noreply.github.com>
2021-03-12 20:42:27 +0300

    Add files via upload

--- Models/MailModels.cs
+++ Models/MailModels.cs
@@ -9,6 +9,7 @@ namespace SipahiDomainCore.Models
         public string Email { get; set; }
 
         [Required(AllowEmptyStrings = false, ErrorMessage = "Yorum giriniz")]
+        [MaxLength(250)]
         public string Message { get; set; }
     }
 }
